960b5b9d19979c0db456739835a6cb6f663633e2
Shay Agroskin <agrosshay@gmail.com>
2020-01-13 20:07:23 +0200

    Changed new line encoding in unpacker module

--- verilog/unpacker.v
+++ verilog/unpacker.v
@@ -1,24 +1,24 @@
 module unpacker(
 	data,
-	seed, 
+	seed,
 	exp,
 	frac
 	);
-	
+
 	// PARAMETERS
 	parameter BITS = 32;
 	parameter ES = 3;
-	
+
 	//INPUT
 	input logic [BITS-1:0] data;
-	
+
 	// OUTPUT
 	output wire signed [BITS-1:0] seed;
 	output wire [ES-1:0] exp;
 	output wire [BITS-1:0] frac;
-	
+
 	wire [BITS - 1:0] shifted_data;
-	
+
 	// FIND THE SEED
 	seed_lookup #(BITS) dut (
 		.data	(data),
@@ -27,6 +27,7 @@ module unpacker(
 	);
 
 	assign exp = $signed(shifted_data[BITS -1 : BITS - ES]);
-	assign frac = $signed(shifted_data[BITS - ES - 1 : 0]);
-	
+	assign frac[BITS-1 : ES] = $signed(shifted_data[BITS - ES - 1 : 0]);
+	assign frac[ES:0] = 0;
+
 endmodule
